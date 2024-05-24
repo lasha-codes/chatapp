15125303f7f82c75cbbf50e815d32450bc7466d0
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-24 22:06:22 +0400

    register page and setting up a backend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
+
+dotenv.config()
 
 app.get('/test', (req, res) => {
   res.json('test ok')
 })
+
+app.post('/register', (req, res) => {})
 
 app.listen(4000, () => {
   console.log('Server running')
