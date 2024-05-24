19e754b422f209b2027ba394eba6297194ceab84
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-24 22:29:15 +0400

    User model and register route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = express()
+const User = require('./models/User.js')
 
 dotenv.config()
+mongoose.connect(process.env.MONGO_URL)
+const jwtSecret = process.env.JWT_SECRET
+
+app.use(express.json())
 
 app.get('/test', (req, res) => {
   res.json('test ok')
 })
 
-app.post('/register', (req, res) => {})
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const createdUser = await User.create({ username, password })
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) throw err
+      res
+        .cookie('token', token)
+        .status(201)
+        .json({ message: 'User has registered an account' })
+    })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
 
 app.listen(4000, () => {
   console.log('Server running')
