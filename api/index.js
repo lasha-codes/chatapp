3137cfc68eda5cd1b4acd1a30d2168800556e7f8
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-25 16:33:41 +0400

    learning even more about websockets and its very interesting

--- api/index.js
+++ api/index.js
@@ -98,7 +98,28 @@ const server = app.listen(4000, () => {
 })
 
 const wss = new ws.WebSocketServer({ server })
-wss.on('connection', (connection) => {
-  console.log('connected')
-  connection.send('hello')
+wss.on('connection', (connection, req) => {
+  const cookies = req.headers.cookie
+  if (cookies) {
+    const tokenCookieString = cookies.split(';').find((str) => {
+      return str.trim().startsWith('token=')
+    })
+    if (tokenCookieString) {
+      const token = tokenCookieString.split('=')[1]
+      if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+          if (err) throw err
+          const { userId, username } = userData
+          connection.userId = userId
+          connection.username = username
+        })
+      }
+    }
+  }
+
+  console.log(
+    [...wss.clients].map((c) => {
+      return c.username
+    })
+  )
 })
