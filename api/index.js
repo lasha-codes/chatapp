93c44e8eabdb5860c2271112134d2f54dffa6b86
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-25 14:20:29 +0400

    on frontend now we have a state on every load gets the user info from the backend

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 const User = require('./models/User.js')
+const cookieParser = require('cookie-parser')
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL)
@@ -17,6 +18,7 @@ app.use(
   })
 )
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/test', (req, res) => {
   res.json('test ok')
@@ -39,13 +41,15 @@ app.post('/register', async (req, res) => {
   const { username, password } = req.body
   try {
     const createdUser = await User.create({ username, password })
-    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
-      if (err) throw err
-      res
-        .cookie('token', token)
-        .status(201)
-        .json({ _id: createdUser._id, username })
-    })
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err
+        res.cookie('token', token).status(201).json({ _id: createdUser._id })
+      }
+    )
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import axios from 'axios'
 
 export const UserContext = createContext({})
 
 export const UserContextProvider = ({ children }) => {
   const [username, setUsername] = useState(null)
   const [id, setId] = useState(null)
+
+  useEffect(() => {
+    try {
+      axios.get('/profile').then((response) => {
+        setId(response.data.userId)
+        setUsername(response.data.username)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }, [])
+
+  console.log({ username, id })
+
   return (
     <UserContext.Provider value={{ username, setUsername, id, setId }}>
       {children}
