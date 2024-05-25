d07880254b9818fbdee907bb63ff2c3d2f87ef5c
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-25 15:45:13 +0400

    dislpaying the chat component if user is logged in

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,13 @@
 import RegisterAndLoginForm from './components/RegisterAndLoginForm'
 import { useContext } from 'react'
 import { UserContext } from './context/UserContext'
+import Chat from './components/Chat'
 
 const Routes = () => {
   const { username } = useContext(UserContext)
 
   if (username) {
-    return 'logged in!' + ' ' + username
+    return <Chat />
   }
 
   return <RegisterAndLoginForm />
